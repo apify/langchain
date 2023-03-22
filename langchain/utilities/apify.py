096e89bb6c86233321285b3612dc3e0b3bf4b67e
Jiří Moravčík <jiri.moravcik@gmail.com>
2023-03-22 15:11:54 +0100

    add option to specify the mapping function

--- langchain/utilities/apify.py
+++ langchain/utilities/apify.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 from pydantic import BaseModel, root_validator
 
@@ -34,7 +34,8 @@ class ApifyWrapper(BaseModel):
         urls: List[str],
         *,
         actor_name: str = "jirimoravcik/content-crawler-copy",
-        run_input: Optional[Dict] = None
+        run_input: Optional[Dict] = None,
+        mapping_function: Optional[Callable[[Dict], Document]] = None,
     ):
         run_input = run_input or {
             "extendOutputFunction": "($) => {\n    const result = {};\n    // Uncomment to add a title to the output\n    // result.pageTitle = $('title').text().trim();\n\n    return result;\n}",
@@ -47,13 +48,16 @@ class ApifyWrapper(BaseModel):
             "maxPagesPerCrawl": 9999999,
             "maxConcurrency": 200,
         }
+        mapping_function = mapping_function or (
+            lambda item: Document(
+                page_content=item["text"], metadata={"source": item["url"]}
+            )
+        )
         actor_call = self.apify_client.actor(actor_name).call(run_input=run_input)
 
         return ApifyDatasetLoader(
             dataset_id=actor_call["defaultDatasetId"],
-            mapping_function=lambda item: Document(
-                page_content=item["text"], metadata={"source": item["url"]}
-            ),
+            mapping_function=mapping_function,
         )
 
     async def arun(
@@ -61,7 +65,8 @@ class ApifyWrapper(BaseModel):
         urls: List[str],
         *,
         actor_name: str = "jirimoravcik/content-crawler-copy",
-        run_input: Optional[Dict] = None
+        run_input: Optional[Dict] = None,
+        mapping_function: Optional[Callable[[Dict], Document]] = None,
     ):
         run_input = run_input or {
             "extendOutputFunction": "($) => {\n    const result = {};\n    // Uncomment to add a title to the output\n    // result.pageTitle = $('title').text().trim();\n\n    return result;\n}",
@@ -74,13 +79,16 @@ class ApifyWrapper(BaseModel):
             "maxPagesPerCrawl": 9999999,
             "maxConcurrency": 200,
         }
+        mapping_function = mapping_function or (
+            lambda item: Document(
+                page_content=item["text"], metadata={"source": item["url"]}
+            )
+        )
         actor_call = await self.apify_client_async.actor(actor_name).call(
             run_input=run_input
         )
 
         return ApifyDatasetLoader(
             dataset_id=actor_call["defaultDatasetId"],
-            mapping_function=lambda item: Document(
-                page_content=item["text"], metadata={"source": item["url"]}
-            ),
+            mapping_function=mapping_function,
         )
